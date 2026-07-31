8a802b54973c4c1ed9e87d2aba1abf91ec6d9ac2
Sergey Mizin <seeergo@yahoo.com>
2026-07-31 23:07:27 +0400

    Turn landing into thematic multi-page site

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,21 @@
+import { apps, services } from "../data/catalog";
+
+const site = "https://lazysoft.ru";
+const staticPaths = ["/", "/services/", "/apps/", "/about/", "/articles/", "/contact/"];
+
+export const GET = () => {
+  const urls = [
+    ...staticPaths,
+    ...services.map((service) => `/services/${service.slug}/`),
+    ...apps.map((app) => `/apps/${app.slug}/`),
+  ];
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map((path) => `  <url><loc>${site}${path}</loc></url>`).join("\n")}
+</urlset>`;
+
+  return new Response(body, {
+    headers: { "Content-Type": "application/xml; charset=utf-8" },
+  });
+};
